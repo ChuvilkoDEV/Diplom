559e83d25c28b6d73512f5cab678835fcdea13ed
ChuvilkoIlya <ilyasutton@gmail.com>
2025-04-24 15:35:47 +0300

    Убрал градиент реализованный в ручную через ::before и ::after на стандартный линейный градиент

--- src/2_pages/HomePage/ui/PopularActivities/PopularActivities.tsx
+++ src/2_pages/HomePage/ui/PopularActivities/PopularActivities.tsx
@@ -6,45 +6,20 @@ import BackgroundWaves from "./BackgroundWaves";
 
 const PopularActivities: React.FC = () => {
   return (
-    <Box sx={{ py: 6, position: "relative", overflow: "hidden" }}>
+    <Box
+      sx={{
+        py: 6,
+        position: "relative",
+        overflow: "hidden",
+        background: `linear-gradient(135deg, rgba(223,25,216,0.5), rgba(153,34,255,0.5))`,
+      }}
+    >
       <BackgroundWaves />
 
-      <Box
-        sx={{
-          position: "relative",
-          mx: "auto",
-          px: 2,
-          "::before": {
-            content: '""',
-            position: "absolute",
-            width: '300%',
-            height: '300%',
-            borderRadius: "50%",
-            background: "radial-gradient(circle, rgba(255,110,196,1), transparent)",
-            top: -100,
-            left: 0,
-            filter: "blur(40px)",
-            zIndex: 0,
-          },
-          "::after": {
-            content: '""',
-            position: "absolute",
-            width: '300%',
-            height: '300%',
-            borderRadius: "50%",
-            background: "radial-gradient(circle, rgba(120,115,245,1), transparent)",
-            bottom: -100,
-            right: 0,
-            filter: "blur(40px)",
-            zIndex: 0,
-          },
-        }}
-      >
-        <Container maxWidth="lg" sx={{ position: "relative", zIndex: 2 }}>
-          <SectionTitle />
-          <ActivitiesGrid />
-        </Container>
-      </Box>
+      <Container maxWidth="lg" sx={{ position: "relative", zIndex: 2 }}>
+        <SectionTitle />
+        <ActivitiesGrid />
+      </Container>
     </Box>
   );
 };
